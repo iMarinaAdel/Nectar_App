4b6127cdafa0eed84e89fad4190bd9a105ab5e1f
marina adel <rinaa1775@gmail.com>
2025-08-16 02:43:55 +0300

    Update Cart

--- lib/features/cart/widget/build_quantity_button.dart
+++ lib/features/cart/widget/build_quantity_button.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:nectar_app/core/utils/app_colors.dart';
 
 class BuildQuantityButton extends StatelessWidget {
-  const BuildQuantityButton({super.key, required this.icon});
-
+  const BuildQuantityButton({super.key, required this.icon, this.itemCount, this.onTap});
+final int? itemCount;
+final void Function()? onTap;
   final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     final bool isAdd = icon == Icons.add;
 
-    return Container(
-      width: 45,
-      height: 45,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-
-        border: Border.all(color: ColorsApp.grayColor.withValues(alpha: .3)),
-      ),
-      child: Icon(
-        icon,
-        color: isAdd ? Colors.green : ColorsApp.grayColor,
-        size: 24,
+    return GestureDetector(
+      onTap:onTap ,
+      child: Container(
+        width: 45,
+        height: 45,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+      
+          border: Border.all(color: ColorsApp.grayColor.withValues(alpha: .3)),
+        ),
+        child: Icon(
+          icon,
+          color: isAdd ? Colors.green : ColorsApp.grayColor,
+          size: 24,
+        ),
       ),
     );
   }
