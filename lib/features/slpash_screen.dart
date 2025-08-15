c24788f878c3a12b86af5e945751913a2b1fe711
marina adel <rinaa1775@gmail.com>
2025-08-15 19:29:06 +0300

    Add favorite feature

--- lib/features/slpash_screen.dart
+++ lib/features/slpash_screen.dart
@@ -3,8 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nectar_app/const/app_assets.dart';
 import 'package:nectar_app/core/utils/app_colors.dart';
 import 'package:nectar_app/extentions/navigation.dart';
-import 'package:nectar_app/features/explore/pages/explore_screen.dart';
-import 'package:nectar_app/features/favorite/pages/favorite_screen.dart';
+import 'package:nectar_app/features/auth/pages/location_screen.dart';
 
 class SlpashScreen extends StatefulWidget {
   const SlpashScreen({super.key});
@@ -17,7 +16,7 @@ class _SlpashScreenState extends State<SlpashScreen> {
   @override
   void initState() {
     Future.delayed(Duration(seconds: 3), () {
-      AppNavigation.pushReplacementTO(context, FavoriteScreen());
+      AppNavigation.pushReplacementTO(context, LocationScreen());
     });
     super.initState();
   }
